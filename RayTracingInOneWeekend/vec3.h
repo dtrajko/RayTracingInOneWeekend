176b3064d5c637b060a80aa177208890f2657a43
Dejan Trajkovic <dtrajko@gmail.com>
2021-02-04 03:00:31 +0100

    vec3.h and color.h classes (2)

--- RayTracingInOneWeekend/vec3.h
+++ RayTracingInOneWeekend/vec3.h
@@ -53,6 +53,7 @@ public:
 using point3 = vec3;   // 3D point
 using color = vec3;    // RGB color
 
+
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
